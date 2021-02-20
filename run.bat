6272e6d9b60ac5f09b452bd0da08fdee23fbbbf5
SivapriyaManikandan <way2priyamail@gmail.com>
2021-02-20 23:59:04 +1100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 call venv\scripts\activate
-rem pytest -v -m "sanity" --html=./Reports/report_chrome.html testCases/ --browser chrome
-pytest -v -m "sanity" --html=./Reports/report_firefox.html testCases/ --browser firefox
+pytest -v -m "sanity" --html=./Reports/report_chrome.html testCases/ --browser chrome
+rem pytest -v -m "sanity" --html=./Reports/report_firefox.html testCases/ --browser firefox
 
 rem chrome
 rem pytest -v -m "sanity or regression" --html=./Reports/report.html testCases/ --browser chrome
